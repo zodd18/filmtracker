c3b2426e9f4b849c104c693ece60a47cd5b27f3f
merlindaniel <merlindaniel30@gmail.com>
2022-01-04 02:16:27 +0100

    RecyclerView moved to FilmsFragment. Better UI/UX

--- app/src/main/java/com/app/filmtracker/FilmsFragment.java
+++ app/src/main/java/com/app/filmtracker/FilmsFragment.java
@@ -1,19 +1,14 @@
 package com.app.filmtracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.ProgressBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,8 +21,6 @@ import com.app.filmtracker.poo.SingletonMap;
 import com.app.filmtracker.reclycler.CustomReclyclerViewAdapter;
 import com.app.filmtracker.vo.Genre;
 import com.app.filmtracker.vo.Movie;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -35,86 +28,96 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FilmsActivity extends AppCompatActivity {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FilmsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FilmsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FilmsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FilmsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FilmsFragment newInstance(String param1, String param2) {
+        FilmsFragment fragment = new FilmsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
 
     //View Components
     RecyclerView filmsRecyclerView;
-    //ProgressBar filmsProgressBar;
-    NavigationBarView navigationBarView;
+
+    //Data
+    List<Genre> genresList;
 
     //Android Volley
     RequestQueue requestQueue;
     Gson gson;
 
-    //Data
-    List<Genre> genresList;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
 
-    //Fragments
-    ProfileFragment profileFragment;
-    FilmsFragment filmsFragment;
-    ChatFragment chatFragment;
+
+
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_films);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_films, container, false);
 
         //Android Volley
         requestQueue = (RequestQueue) SingletonMap.getInstance().get(SingletonMap.REQUEST_QUEUE);
         if(requestQueue == null){
-            requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue = Volley.newRequestQueue(getActivity());
             SingletonMap.getInstance().put(SingletonMap.REQUEST_QUEUE, requestQueue);
         }
 
-        //View Components
-        //filmsRecyclerView = findViewById(R.id.filmsRecyclerView);
-        //filmsProgressBar = findViewById(R.id.filmsProgressBar);
-        //filmsProgressBar.setIndeterminate(true);
-        //filmsProgressBar.onVisibilityAggregated(true);
-
-        //Fragments
-        profileFragment = new ProfileFragment();
-        filmsFragment = new FilmsFragment();
-        chatFragment = new ChatFragment();
-
         //Recycler View
+        filmsRecyclerView = view.findViewById(R.id.filmsRecyclerView);
+        genresList = new ArrayList<>();
         gson = new Gson();
-        //fetchGenreAndThenStartRecyclerView();
-
-        //Bottom Menu
-        navigationBarView = findViewById(R.id.MainBottomNavigation);
-        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.bottomMenuProfile:
-                        loadFragment(profileFragment);
-                        return true;
-                    case R.id.bottomMenuFilms:
-                        loadFragment(filmsFragment);
-                        return true;
-                    case R.id.bottomMenuChat:
-                        loadFragment(chatFragment);
-                        return true;
-
-                }
-                return false;
-            }
-        });
-
-
+        fetchGenreAndThenStartRecyclerView();
+        return view;
     }
 
-    private void loadFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.mainFrameContainer, fragment);
-        fragmentTransaction.commit();
-    }
+
+
+
 
     private void fetchGenreAndThenStartRecyclerView(){
         String url = "https://api.themoviedb.org/3/genre/movie/list?api_key=a9e15ccf0b964bbf599fef3ba94ef87b&language=es";
@@ -146,9 +149,9 @@ public class FilmsActivity extends AppCompatActivity {
     }
 
     private void createAndStartRecyclerView(){
-        filmsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2)); //new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        filmsRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2)); //new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
-        CustomReclyclerViewAdapter adapter = new CustomReclyclerViewAdapter(this, genresList);
+        CustomReclyclerViewAdapter adapter = new CustomReclyclerViewAdapter(getActivity(), genresList);
 
         adapter.setOnLoadCustomListener(new OnLoadCustomListener() {
             @Override
@@ -185,7 +188,4 @@ public class FilmsActivity extends AppCompatActivity {
 
         filmsRecyclerView.setAdapter(adapter);
     }
-
-
-
 }
