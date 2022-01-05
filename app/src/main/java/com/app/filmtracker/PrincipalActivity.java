2ec723d5dc03116870f1d559ef2c81020cb1a7d6
merlindaniel <merlindaniel30@gmail.com>
2022-01-05 15:03:38 +0100

    Events of top menu defined

--- app/src/main/java/com/app/filmtracker/PrincipalActivity.java
+++ app/src/main/java/com/app/filmtracker/PrincipalActivity.java
@@ -2,14 +2,18 @@ package com.app.filmtracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -19,6 +23,7 @@ public class PrincipalActivity extends AppCompatActivity {
 
     //View Components
     NavigationBarView navigationBarView;
+    MaterialToolbar topMenu;
 
     //Fragments
     ProfileFragment profileFragment;
@@ -35,9 +40,11 @@ public class PrincipalActivity extends AppCompatActivity {
         filmsFragment = new FilmsFragment();
         likesFragment = new LikesFragment();
 
-        //Bottom Menu
+        //View Components
         navigationBarView = findViewById(R.id.MainBottomNavigation);
+        topMenu = findViewById(R.id.principalTopMenu);
 
+        //Bottom Menu
         loadFragment(filmsFragment);
         navigationBarView.setSelectedItemId(R.id.bottomMenuFilms);
         navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -60,6 +67,21 @@ public class PrincipalActivity extends AppCompatActivity {
         });
 
 
+        //Top menu
+        topMenu.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.topMenuChat:
+                        System.out.println("--------entra en chat");
+                        return true;
+                    case R.id.topMenuSearch:
+                        System.out.println("--------entra en la busqueda");
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment){
@@ -67,77 +89,5 @@ public class PrincipalActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.mainFrameContainer, fragment);
         fragmentTransaction.commit();
     }
-
-   /* private void fetchGenreAndThenStartRecyclerView(){
-        String url = "https://api.themoviedb.org/3/genre/movie/list?api_key=a9e15ccf0b964bbf599fef3ba94ef87b&language=es";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray genresJsonList = response.getJSONArray("genres");
-                            Type listType = new TypeToken<ArrayList<Genre>>(){}.getType();
-                            genresList = gson.fromJson(genresJsonList.toString(), listType);
-
-                            System.out.println(response.toString());
-                            createAndStartRecyclerView();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-                        System.out.println("------Error en el Volley");
-                    }
-                });
-        requestQueue.add(jsonObjectRequest);
-    }
-
-    private void createAndStartRecyclerView(){
-        filmsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2)); //new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-
-        CustomReclyclerViewAdapter adapter = new CustomReclyclerViewAdapter(this, genresList);
-
-        adapter.setOnLoadCustomListener(new OnLoadCustomListener() {
-            @Override
-            public void load() {
-                String url = "https://api.themoviedb.org/3/discover/movie?api_key=a9e15ccf0b964bbf599fef3ba94ef87b&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page="+ adapter.getLastPage() +"&with_watch_monetization_types=flatrate";
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                try {
-                                    adapter.setLastPage(response.getInt("page") + 1);
-                                    JSONArray movieJsonList = response.getJSONArray("results");
-                                    Type listType = new TypeToken<ArrayList<Movie>>(){}.getType();
-                                    List<Movie> movList = gson.fromJson(movieJsonList.toString(), listType);
-                                    adapter.addNewDataAndNotify(movList);
-
-                                    System.out.println(response.toString());
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-                        }, new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // TODO: Handle error
-                                System.out.println("------Error en el Volley");
-                            }
-                        });
-                requestQueue.add(jsonObjectRequest);
-            }
-        });
-
-        filmsRecyclerView.setAdapter(adapter);
-    }*/
-
-
 
 }
